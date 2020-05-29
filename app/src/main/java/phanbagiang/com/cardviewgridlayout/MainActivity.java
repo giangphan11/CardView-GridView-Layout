4e2d9d1a152f31230e2a9e4b9f7334daf8bb49e2
Giang Phan <gphan485@gmail.com>
2020-05-29 17:05:06 +0700

    add 2 more functions

--- app/src/main/java/phanbagiang/com/cardviewgridlayout/MainActivity.java
+++ app/src/main/java/phanbagiang/com/cardviewgridlayout/MainActivity.java
@@ -1,14 +1,20 @@
 package phanbagiang.com.cardviewgridlayout;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.widget.Toast;
+
+import com.google.android.material.navigation.NavigationView;
 
 import java.util.ArrayList;
 
@@ -21,7 +27,11 @@ public class MainActivity extends AppCompatActivity {
     ArrayList<Book>dsBook;
 
     DrawerLayout drawerLayout;
+    NavigationView navigationView;
     ActionBarDrawerToggle drawerToggle;
+
+    FragmentManager fragmentManager=null;
+    FragmentTransaction fragmentTransaction=null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +39,34 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         fakeData();
         addControls();
+        addEvents();
+    }
+
+    private void addEvents() {
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                xuLyItem(menuItem);
+                return false;
+            }
+        });
+    }
+    private void xuLyItem(MenuItem menuItem){
+        fragmentManager=getSupportFragmentManager();
+        fragmentTransaction=fragmentManager.beginTransaction();
+        Fragment fragment=null;
+        switch (menuItem.getItemId()){
+            case R.id.mnuAbout:
+                fragment=new FragmentAbout();
+                break;
+            case R.id.mnuHelp:
+                fragment=new FragmentHelp();
+                break;
+            default:
+                return;
+        }
+        fragmentTransaction.replace(R.id.myFrameLayout,fragment).commit();
+        drawerLayout.closeDrawers();
     }
 
     private void addControls() {
@@ -36,6 +74,7 @@ public class MainActivity extends AppCompatActivity {
         drawerToggle=new ActionBarDrawerToggle(this,drawerLayout,R.string.open,R.string.close);
         drawerLayout.addDrawerListener(drawerToggle);
         drawerToggle.syncState();
+        navigationView=findViewById(R.id.myNavigationview);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
@@ -46,15 +85,15 @@ public class MainActivity extends AppCompatActivity {
         //RecyclerView.LayoutManager layoutManager=new GridLayoutManager(this,3);
 
         recBook.setLayoutManager(new GridLayoutManager(this,3));
+
+
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         if(drawerToggle.onOptionsItemSelected(item))
             return true;
-        if(item.getItemId()==R.id.mnuAbout){
-            Toast.makeText(this,"OK",Toast.LENGTH_SHORT).show();
-        }
+
         return super.onOptionsItemSelected(item);
     }
 
@@ -85,4 +124,5 @@ public class MainActivity extends AppCompatActivity {
         dsBook.add(new Book(R.drawable.thevigitarian,"Thevigitarian","Use it as a view group to contain other views."));
         dsBook.add(new Book(R.drawable.thewildrobot,"Thewildrobot","Use it as a view group to contain other views."));
     }
+
 }
